b7357f8cf32b55b44087ea63bd2fb3cdafa35759
phbr13 <phborges2006@gmail.com>
2024-01-01 19:07:16 -0300

    update server.js para verificar se senha está correta

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const senhaAdm = process.env.SENHA_ADM
 
 const express = require('express');
 const app = express();
-const porta = 3000;
+const porta = 1313;
 
 // Middleware para analisar corpos JSON
 app.use(express.json());
@@ -14,23 +14,40 @@ app.use(express.json());
 // Middleware para analisar dados de formulário URL-encoded
 app.use(express.urlencoded({ extended: true }));
 
-app.post('/exemplo-rota', (req, res) => {
+// Configurando CORS
+app.use((req, res , next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header(
+        'Access-Control-Allow-Header',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    )
+    if(req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Headers', 'Content-Type')
+        res.header('Access-Control-Allow-Methods', 'PUT, PATCH, DELETE, GET, POST')
+        return res.status(200).send({})
+    }
+    next()
+})
+
+// Recebendo requisição de POST para ver se senha está correta
+app.post('/senha_adm', (req, res) => {
   try {
-    // Verificar se há dados no corpo da solicitação
     if (!req.body) {
       return res.status(400).json({ erro: 'Corpo da solicitação vazio' });
     }
-    // O corpo da solicitação está disponível em req.body
     const dadosDoCorpo = req.body;
-    console.log(dadosDoCorpo);
-
-    res.status(200).send('Solicitação recebida com sucesso!');
+    if (dadosDoCorpo.senha == senhaAdm) {
+        res.status(200).json({acerto:true});
+    } else {
+        res.status(200).json({acerto:false});
+    }
   } catch (erro) {
     console.error('Erro ao processar a solicitação:', erro);
     res.status(500).json({ erro: 'Erro interno do servidor' });
   }
 });
 
+// Ouvindo porta no 1313
 app.listen(porta, () => {
   console.log(`Servidor rodando na porta ${porta}`);
 });
