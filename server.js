b2d149579b743b9029a06c0cd8a08f43da5d8950
phbr13 <phborges2006@gmail.com>
2024-01-03 18:54:36 -0300

    update para adptar ao front-end(mudou na pesquisa de filmes alocada a TMDB API)

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res , next) => {
     res.header('Access-Control-Allow-Origin', '*')
     res.header(
-        'Access-Control-Allow-Header',
+        'Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     )
     if(req.method === 'OPTIONS') {
